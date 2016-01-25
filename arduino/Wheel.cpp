09262fc0ac00cfc1836d8e62d29a95527540c9a7
Joan Rieu <joan.rieu@gmail.com>
2016-01-25 13:11:11 +0100

    Refactored PID constants

--- arduino/Wheel.cpp
+++ arduino/Wheel.cpp
@@ -6,12 +6,14 @@
 
 #define ENCODER_INCREMENTS_PER_TURN 3072
 #define POSITION_DELTA_COUNT (sizeof(mPositionDeltas) / sizeof(mPositionDeltas[0]))
+#define INCREMENTS_TO_RADIANS (2 * M_PI / ENCODER_INCREMENTS_PER_TURN)
+#define FIXED_POINT_SHIFT 7
 
 Wheel::Wheel(Motor& motor, RotaryEncoder& encoder, float updateFrequency):
     mMotor(motor),
     mEncoder(encoder),
     mUpdateFrequency(updateFrequency),
-    mKP(50), mKI(10), mKD(50),
+    mKP(50), mKI(1), mKD(0),
     mPositionDeltas{0},
     mPositionDeltaIndex(0),
     mPositionDeltaSum(0),
@@ -27,7 +29,7 @@ void Wheel::updateOdometry() {
 float Wheel::getAngularSpeed() const {
     return mPositionDeltaSum
         * mUpdateFrequency
-        * (2 * M_PI / ENCODER_INCREMENTS_PER_TURN)
+        * INCREMENTS_TO_RADIANS
         / POSITION_DELTA_COUNT;
 }
 
@@ -35,29 +37,25 @@ void Wheel::setTargetSpeed(float speed) {
     mPositionDeltaSumTarget =
         speed
         * POSITION_DELTA_COUNT
-        / (2 * M_PI / ENCODER_INCREMENTS_PER_TURN)
+        / INCREMENTS_TO_RADIANS
         / mUpdateFrequency;
     mPositionDeltaSumIntegratedError = 0;
 }
 
-
 void Wheel::regulatePower() {
-
     const int error = mPositionDeltaSumTarget - mPositionDeltaSum;
     mPositionDeltaSumIntegratedError += error;
     mPositionDeltaSumIntegratedError =
-        mPositionDeltaSumIntegratedError > 12700 ? 12700 :
-        mPositionDeltaSumIntegratedError < -12700 ? -12700 :
+        mPositionDeltaSumIntegratedError > (127 << FIXED_POINT_SHIFT) ? (127 << FIXED_POINT_SHIFT) :
+        mPositionDeltaSumIntegratedError < (-127 << FIXED_POINT_SHIFT) ? (-127 << FIXED_POINT_SHIFT) :
         mPositionDeltaSumIntegratedError;
     const int p = mKP * error;
     const int i = mKI * mPositionDeltaSumIntegratedError;
     const int d = mKD * mPositionDeltas[mPositionDeltaIndex];
-    const int output = -(p + i - d) >> 7;
+    const int output = -(p + i - d) >> FIXED_POINT_SHIFT;
     mMotor.setPower(
         output > 127 ? 127 :
         output < -127 ? -127 :
         output
     );
-
-
 }
